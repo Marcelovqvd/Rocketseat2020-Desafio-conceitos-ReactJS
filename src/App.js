67aac42cf8a25ed14d711396c1cbef877d7162a8
Marcelo <marcelovqvd@gmail.com>
2020-06-22 11:34:25 -0300

    doing

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import api from './services/api';
 import "./styles.css";
 
 function App() {
 
-  const [repositories, setRepositories] = useState([])
-
-  useEffect(() => {
-    api.get('/repositories').then(response => {
-      setRepositories(response.data)
-    })
-  }, [])
+  const [repositories, setRepositories] = useState([]);
 
   async function handleAddRepository() {
-    const response = await api.post('/repositories', {
-      title: "Node.js",
+    const repository = await api.post('/repositories', {
+      title: "Novo"
     })
-    const project = response.data;
 
-    setRepositories([...repositories, project]);
+    setRepositories([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // todo
+    await api.delete(`/repositories/${id}`);
+    const newRepositories = repositories.filter((repository) => repository.id !== id);
+    console.log(newRepositories)
   }
+
+  async function handleShowRepository(id) {    
+    console.log(repositories)
+  }
+  
 
   return (
     <div>
       <ul data-testid="repository-list">
         {repositories.map(repository => (
-          <li key={repository.id}>
-            {repository.title}
-            <button onClick={() => handleRemoveRepository(repository.id)}>
-              Remover
-            </button>
-          </li>
+          <li key={repository.data.id}>
+          <h3>Title: {repository.data.title} - </h3>
+          <p> id {repository.data.id}</p>
+          <button onClick={() => handleRemoveRepository(repository.data.id)}>
+            Remover
+          </button>
+          {/* <button onClick={handleShowActualRepository}>Mostra log do atual</button> */}
+        </li>
         ))}
       </ul>
-
       <button onClick={handleAddRepository}>Adicionar</button>
+      <p></p>
+      <button onClick={handleShowRepository}>Mostra log</button>
     </div>
   );
 }
