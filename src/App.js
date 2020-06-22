cac86ed9072f3ac1ae83fe00282d662d7bab6f77
Marcelo <marcelovqvd@gmail.com>
2020-06-22 16:17:39 -0300

    post and delete

--- src/App.js
+++ src/App.js
@@ -5,43 +5,35 @@ import "./styles.css";
 function App() {
 
   const [repositories, setRepositories] = useState([]);
-
+  
   async function handleAddRepository() {
-    const repository = await api.post('/repositories', {
-      title: "Novo"
-    })
-
-    setRepositories([...repositories, repository]);
+   const repository = await api.post('/repositories', {
+     title: 'Node.js',
+   });
+   const newRepository = repository.data;
+   
+   setRepositories([...repositories, newRepository])
   }
 
   async function handleRemoveRepository(id) {
     await api.delete(`/repositories/${id}`);
-    const newRepositories = repositories.filter((repository) => repository.id !== id);
-    console.log(newRepositories)
-  }
-
-  async function handleShowRepository(id) {    
-    console.log(repositories)
-  }
-  
+    const newRepositories = repositories.filter(repository => repository.id !== id);
+    setRepositories(newRepositories);
+  }  
 
   return (
     <div>
       <ul data-testid="repository-list">
         {repositories.map(repository => (
-          <li key={repository.data.id}>
-          <h3>Title: {repository.data.title} - </h3>
-          <p> id {repository.data.id}</p>
-          <button onClick={() => handleRemoveRepository(repository.data.id)}>
-            Remover
-          </button>
-          {/* <button onClick={handleShowActualRepository}>Mostra log do atual</button> */}
-        </li>
-        ))}
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>   
+        ))}        
       </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
-      <p></p>
-      <button onClick={handleShowRepository}>Mostra log</button>
     </div>
   );
 }
