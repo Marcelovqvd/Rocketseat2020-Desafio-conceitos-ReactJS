df07ff1a268f0d93e43f9b39b8ae771f9899255e
Marcelo <marcelovqvd@gmail.com>
2020-06-23 17:25:57 -0300

    fix test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import api from './services/api';
 import "./styles.css";
 
 function App() {
 
   const [repositories, setRepositories] = useState([]);
+
+  useEffect( () => {
+    api.get('/repositories').then( ({ data }) => {
+      setRepositories(data);
+    });
+  }, []);
   
   async function handleAddRepository() {
    const repository = await api.post('/repositories', {
@@ -24,14 +30,14 @@ function App() {
   return (
     <div>
       <ul data-testid="repository-list">
-        {repositories.map(repository => (
+        {repositories.map(repository =>
           <li key={repository.id}>
             {repository.title}
             <button onClick={() => handleRemoveRepository(repository.id)}>
               Remover
             </button>
           </li>   
-        ))}        
+        )}        
       </ul>
       <button onClick={handleAddRepository}>Adicionar</button>
     </div>
